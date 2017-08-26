4253060875e7e8d0ad3e74cb28125820c02c7c7c
Narazaka <info@narazaka.net>
2017-08-27 02:44:57 +0900

    kiznit/uefi-headers ベースにする

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 all: fs/EFI/BOOT/BOOTX64.EFI
 
-fs/EFI/BOOT/BOOTX64.EFI: main.cpp
+fs/EFI/BOOT/BOOTX64.EFI: main.cpp include/ProcessorBind.h
 	mkdir -p fs/EFI/BOOT
-	x86_64-w64-mingw32-g++ -Wall -Wextra -e efi_main -nostdlib \
+	x86_64-w64-mingw32-g++ -Wall -Wextra -e efi_main -Iuefi-headers/Include -Iinclude -nostdlib \
 	-fno-builtin -Wl,--subsystem,10 -o $@ $<
+
+include/ProcessorBind.h:
+	mkdir -p include
+	cp uefi-headers/Include/X64/ProcessorBind.h include/ProcessorBind.h
 
 run: fs/EFI/BOOT/BOOTX64.EFI
 	qemu-system-x86_64 -bios C:/usr/OVMF/OVMF.fd -hda fat:fs
 
 clean:
 	rm fs/EFI/BOOT/BOOTX64.EFI
+	rm include/ProcessorBind.h
 
 .PHONY: clean
--- main.cpp
+++ main.cpp
@@ -1,35 +1,8 @@
-#include <stdint.h>
-typedef int8_t INT8;
-typedef uint8_t UINT8;
-typedef int16_t INT16;
-typedef uint16_t UINT16;
-typedef int32_t INT32;
-typedef uint32_t UINT32;
-typedef int64_t INT64;
-typedef uint64_t UINT64;
-typedef int8_t CHAR8;
-typedef int16_t CHAR16;
-typedef void VOID;
-// if 64bit
-typedef UINT64 UINTN;
-typedef UINTN EFI_STATUS;
-
-struct EFI_SYSTEM_TABLE {
-  char _buf[60];
-  struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    uint64_t _buf;
-    uint64_t (*OutputString)(
-        struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
-        uint16_t *String);
-    uint64_t _buf2[4];
-    uint64_t (*ClearScreen)(
-        struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This);
-  } *ConOut;
-};
+#include <Uefi.h>
 
 extern "C" void efi_main(void *ImageHandle __attribute__ ((unused)),
-    struct EFI_SYSTEM_TABLE *SystemTable) {
+    EFI_SYSTEM_TABLE *SystemTable) {
   SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, (uint16_t*)L"Hello!\r\nUEFI!\r\n");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, (CHAR16*)L"Hello!\r\nUEFI!\r\n");
   while (1);
 }
